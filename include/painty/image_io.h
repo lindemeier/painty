bf6e204c0c6d802206c6e2ed11d09a72eb61f802
tsl <thomas.lindemeier@mojin-robotics.de>
2020-04-22 14:45:48 +0200

    adds function rumps for image io

--- include/painty/image_io.h
+++ include/painty/image_io.h
@@ -1,8 +1,31 @@
+/**
+ * @file image_io.cpp
+ * @author thomas lindemeier
+ *
+ * @brief
+ *
+ * @date 2020-04-22
+ *
+ */
 #ifndef PAINTY_IMAGE_IO_H
 #define PAINTY_IMAGE_IO_H
 
+#include <painty/mat.h>
+#include <painty/vec.h>
+
 namespace painty
 {
+namespace io
+{
+bool imRead(const std::string& filename, Mat<vec3>& srgb);
+
+bool imRead(const std::string& filename, Mat<double>& gray);
+
+bool imSave(const std::string& filename, const Mat<vec3>& srgb);
+
+bool imSave(const std::string& filename, const Mat<double>& gray);
+}  // namespace io
+
 }  // namespace painty
 
 #endif  // PAINTY_IMAGE_IO_H
--- src/image_io.cpp
+++ src/image_io.cpp
@@ -2,14 +2,33 @@
 
 #include <string>
 
-namespace painty
+#include <png.h>
+
+bool painty::io::imRead(const std::string& filename, Mat<vec3>& srgb)
 {
-// void imLoad(const std::string& filename, Mat3d& image)
-// {
-// }
+  FILE* fp = fopen(filename.c_str(), "rb");
 
-// void imSave(const std::string& filename, Mat1d& image)
-// {
-// }
+  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png)
+    abort();
 
-}  // namespace painty
+  png_infop info = png_create_info_struct(png);
+  if (!info)
+  {
+    throw std::runtime_error("could not create png info struct");
+  }
+
+  return true;
+}
+
+bool painty::io::imRead(const std::string& filename, Mat<double>& gray)
+{
+}
+
+bool painty::io::imSave(const std::string& filename, const Mat<vec3>& srgb)
+{
+}
+
+bool painty::io::imSave(const std::string& filename, const Mat<double>& gray)
+{
+}
