172aa82357e5756ecfbce46c78954b2c17ecc2e6
tsl <thomas.lindemeier@mojin-robotics.de>
2020-04-23 21:09:50 +0200

    testing ramp

--- src/image_io.cpp
+++ src/image_io.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 
 #include <png.h>
+#include <ColorConverter/ColorConverter.hxx>
 
 bool painty::io::imRead(const std::string& filename, Mat<vec3>& linear_rgb)
 {
@@ -55,6 +56,47 @@ bool painty::io::imRead(const std::string& filename, Mat<vec3>& linear_rgb)
 
 bool painty::io::imRead(const std::string& filename, Mat<double>& gray)
 {
+  png_image png_image; /* The control structure used by libpng */
+                       /* Initialize the 'png_image' structure. */
+  std::memset(&png_image, 0, (sizeof png_image));
+  png_image.version = PNG_IMAGE_VERSION;
+
+  gray = Mat<double>();
+
+  auto success = false;
+
+  if (png_image_begin_read_from_file(&png_image, filename.c_str()) != 0)
+  {
+    png_bytep buffer = nullptr;
+
+    // 1 channel 16bit depth
+    png_image.format = PNG_FORMAT_LINEAR_Y;
+
+    buffer = static_cast<png_bytep>(std::malloc(PNG_IMAGE_SIZE(png_image)));
+
+    if (buffer != nullptr && png_image_finish_read(&png_image, nullptr, buffer, 0, nullptr) != 0)
+    {
+      // process
+      gray = painty::Mat<double>(png_image.height, png_image.width);
+
+      constexpr auto scale = 1.0 / static_cast<double>(0xFFFF);
+      auto& data = gray.getData();
+      for (auto i = 0U; i < data.size(); i++)
+      {
+        auto px_ptr = reinterpret_cast<png_uint_16p>(&(buffer[i * 2U]));
+
+        data[i] = static_cast<double>(px_ptr[0]) * scale;
+      }
+      success = true;
+    }
+
+    png_image_free(&png_image);
+    if (buffer)
+    {
+      free(buffer);
+    }
+  }
+  return success;
 }
 
 bool painty::io::imSave(const std::string& filename, const Mat<vec3>& linear_rgb)
@@ -92,4 +134,31 @@ bool painty::io::imSave(const std::string& filename, const Mat<vec3>& linear_rgb
 
 bool painty::io::imSave(const std::string& filename, const Mat<double>& gray)
 {
+  auto success = false;
+
+  png_image png_image;
+  memset(&png_image, 0, sizeof(png_image));
+  png_image.version = PNG_IMAGE_VERSION;
+
+  png_image.width = gray.getCols();
+  png_image.height = gray.getRows();
+  png_image.format = PNG_FORMAT_LINEAR_Y;
+
+  const auto clamp = [](const double a, const double minv, const double maxv) {
+    return std::min(maxv, std::max(minv, a));
+  };
+
+  const auto& image_data = gray.getData();
+  std::vector<png_uint_16> bit16_data(image_data.size());
+  constexpr auto scale = static_cast<double>(0xFFFF);
+  for (auto i = 0U; i < image_data.size(); i++)
+  {
+    auto px_ptr = reinterpret_cast<png_uint_16p>(&(bit16_data[i]));
+
+    px_ptr[0] = static_cast<png_uint_16>(clamp(image_data[i] * scale, 0U, 0xFFFF));
+  }
+
+  success = png_image_write_to_file(&png_image, filename.c_str(), 0, bit16_data.data(), 0, NULL);
+
+  return success;
 }
--- test/image_io_test.cpp
+++ test/image_io_test.cpp
@@ -12,15 +12,20 @@
 
 #include <painty/painty.h>
 
-TEST(ImageIoTest, ReadWrite)
+TEST(ImageIoTest, ReadWriteRGB)
 {
-  painty::Mat<painty::vec3> linear_rgb(1024, 768);
+  painty::Mat<painty::vec3> linear_rgb(256, 1024);
 
-  constexpr painty::vec3 color = { 0.5, 0.001, 0.999 };
-
-  for (auto& pixel : linear_rgb.getData())
+  constexpr auto steps = 20U;
+  for (uint32_t i = 0; i < linear_rgb.getRows(); i++)
   {
-    pixel = color;
+    for (uint32_t j = 0; j < linear_rgb.getCols(); j++)
+    {
+      const double p = j / static_cast<double>(linear_rgb.getCols() - 1U);
+      const double a = std::floor(static_cast<uint32_t>(p * steps));
+      const auto v = a / steps;
+      linear_rgb(i, j) = { v, v, v };
+    }
   }
   painty::io::imSave("linear_rgb.png", linear_rgb);
 
@@ -38,6 +43,39 @@ TEST(ImageIoTest, ReadWrite)
       EXPECT_NEAR(linear_rgb(i, j)[0], linear_rgb_read(i, j)[0], eps);
       EXPECT_NEAR(linear_rgb(i, j)[1], linear_rgb_read(i, j)[1], eps);
       EXPECT_NEAR(linear_rgb(i, j)[2], linear_rgb_read(i, j)[2], eps);
+    }
+  }
+}
+
+TEST(ImageIoTest, ReadWriteSingle)
+{
+  painty::Mat<double> luminance(256, 1024);
+
+  constexpr auto steps = 20U;
+  for (uint32_t i = 0; i < luminance.getRows(); i++)
+  {
+    for (uint32_t j = 0; j < luminance.getCols(); j++)
+    {
+      const double p = j / static_cast<double>(luminance.getCols() - 1U);
+      const double a = std::floor(static_cast<uint32_t>(p * steps));
+      const auto v = a / steps;
+      luminance(i, j) = v;
+    }
+  }
+  painty::io::imSave("luminance.png", luminance);
+
+  painty::Mat<double> luminance_read;
+  painty::io::imRead("luminance.png", luminance_read);
+
+  EXPECT_EQ(luminance_read.getCols(), luminance.getCols());
+  EXPECT_EQ(luminance_read.getRows(), luminance.getRows());
+
+  constexpr double eps = 0.001;
+  for (uint32_t i = 0; i < luminance.getRows(); i++)
+  {
+    for (uint32_t j = 0; j < luminance.getCols(); j++)
+    {
+      EXPECT_NEAR(luminance(i, j), luminance_read(i, j), eps);
     }
   }
 }
