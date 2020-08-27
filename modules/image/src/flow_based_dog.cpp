78d9c2e1a87cbdbdad2a66fdaf1327c2fb6bb6b1
tsl <thomas.lindemeier@mojin-robotics.de>
2020-08-27 17:42:51 +0200

    adds flow filter implementation

--- modules/image/src/flow_based_dog.cpp
+++ modules/image/src/flow_based_dog.cpp
@@ -80,13 +80,154 @@ static void run_oabf(uint32_t pass, const Mat3d& sourceLab, Mat3d& target,
     }
   }
 }
+
+static void fdogAlongGradient(const Mat1d& img, Mat1d& dst, const Mat2d& tfm,
+                              const double sigma_e, const double sigma_r,
+                              const double tau) {
+  const auto twoSigmaESquared = 2.0 * sigma_e * sigma_e;
+  const auto twoSigmaRSquared = 2.0 * sigma_r * sigma_r;
+
+  const auto w = img.cols;
+  const auto h = img.rows;
+
+  for (auto y = 0; y < h; y++) {
+    for (auto x = 0; x < w; x++) {
+      const vec2 uv = {x, y};
+      const auto t  = tfm(clamp(static_cast<int32_t>(round(uv[1U])), 0, h - 1),
+                         clamp(static_cast<int32_t>(round(uv[0U])), 0,
+                               w - 1));  // nearest neighbor
+      vec2 n        = {t[1U], -t[0U]};    // along gradient not tangent
+      if (std::fabs(n[0U]) >= std::fabs(n[1U])) {
+        n[1U] = n[1U] / n[0U];
+        n[0U] = 1.0;
+      } else {
+        n[0U] = n[0U] / n[1U];
+        n[1U] = 1.0;
+      }
+      const auto ht = Interpolate(img, uv);
+
+      auto sumG0  = ht;
+      auto sumG1  = ht;
+      auto normG0 = 1.0;
+      auto normG1 = 1.0;
+
+      auto halfWidth = 2.0 * sigma_r / sqrt(n[0U] * n[0U] + n[1U] * n[1U]);
+      for (auto d = 1; d <= halfWidth; d++) {
+        // kernel for both gaussians
+        vec2 kernel = {exp(-d * d / twoSigmaESquared),
+                       exp(-d * d / twoSigmaRSquared)};
+        normG0 += 2.0 * kernel[0U];
+        normG1 += 2.0 * kernel[1U];
+
+        const auto backwardsValue =
+          Interpolate(img, {uv[0U] - d * n[0U], uv[1U] - d * n[1U]});
+        const auto forwardsValue =
+          Interpolate(img, {uv[0U] + d * n[0U], uv[1U] + d * n[1U]});
+
+        // only Luminance used
+        const auto accumValues = backwardsValue + forwardsValue;
+
+        sumG0 += kernel[0] * accumValues;
+        sumG1 += kernel[1] * accumValues;
+      }
+      sumG0 /= normG0;
+      sumG1 /= normG1;
+
+      // DoG operation
+      dst(y, x) = sumG0 - tau * sumG1;
+    }
+  }
+}
+
+static void smoothAlongFlow(const Mat1d& img, Mat1d& dst, const Mat2d& tfm,
+                            const double sigma_m) {
+  struct lic_t {
+    vec2 p    = {0.0, 0.0};
+    vec2 t    = {0.0, 0.0};
+    double w  = 0.0;
+    double dw = 0.0;
+  };
+  const auto sign = [](double x) {
+    return (x <= 0.0) ? -1.0 : 1.0;
+  };
+
+  const auto step = [sign, &tfm](lic_t& s) {
+    auto t = Interpolate(tfm, {s.p[0U], s.p[1U]});
+    if (t.dot(s.t) < 0.0)
+      t *= -1.0;
+    s.t[0U] = t[0U];
+    s.t[1U] = t[1U];
+
+    s.dw = (std::fabs(t[0U]) >= std::fabs(t[1U]))
+             ? std::fabs(((s.p[0U] - std::floor(s.p[0U])) - 0.5 - sign(t[0U])) /
+                         t[0U])
+             : std::fabs(((s.p[1U] - std::floor(s.p[1U])) - 0.5 - sign(t[1U])) /
+                         t[1U]);
+
+    s.p[0U] += t[0U] * s.dw;
+    s.p[1U] += t[1U] * s.dw;
+    s.w += s.dw;
+  };
+
+  const double twoSigmaMSquared = 2.0 * sigma_m * sigma_m;
+  const double halfWidth        = 2.0 * sigma_m;
+  const auto w                  = img.cols;
+  const auto h                  = img.rows;
+
+  for (auto y = 0; y < h; y++) {
+    for (auto x = 0; x < w; x++) {
+      const vec2 uv = {x, y};
+      double wg     = 1.0;
+      double H      = img(y, x);
+
+      lic_t a, b;
+      a.p[0U] = b.p[0U] = uv[0U];
+      a.p[1U] = b.p[1U] = uv[1U];
+      a.t               = tfm(y, x);
+      b.t               = -tfm(y, x);
+      a.w = b.w = 0.0;
+
+      while (a.w < halfWidth) {
+        step(a);
+        double k = a.dw * exp(-a.w * a.w / twoSigmaMSquared);
+        H += k * Interpolate(img, {a.p[0U], a.p[1U]});
+        wg += k;
+      }
+      while (b.w < halfWidth) {
+        step(b);
+        double k = b.dw * exp(-b.w * b.w / twoSigmaMSquared);
+        H += k * Interpolate(img, {b.p[0U], b.p[1U]});
+        wg += k;
+      }
+      H /= wg;
+
+      dst(y, x) = H;
+    }
+  }
+}
+
 }  // namespace detail
 
-Mat3d FlowBasedDoG::execute(const Mat3d& imageInLab,
-                            const Mat2d& edgeTangentFlow) const {
-  filterBilateralOrientationAligned(imageInLab, edgeTangentFlow, _oabfSigma_d,
-                                    _oabfSigma_r, _oabfIterations);
-  return quantizeColors(imageInLab, _phi_q, _nbins);
+Mat3d FlowBasedDoG::execute(const Mat3d& rgbLinear) const {
+  const auto Lab =
+    convertColor(rgbLinear, ColorConverter<double>::Conversion::rgb_2_CIELab);
+  const auto etf = ComputeEdgeTangentFlow(
+    tensor::ComputeTensors(Lab, Mat1d(), 0.0, _tensorOuterSigma, 0.0, 0.0));
+
+  const auto oabf = filterBilateralOrientationAligned(
+    Lab, etf, _oabfSigma_d, _oabfSigma_r, _oabfIterations);
+
+  std::vector<Mat1d> channels;
+  cv::split(oabf, channels);
+  const auto dogResponse = filterFlowBasedDoG(
+    channels.front(), etf, _xdogParamSigma, _xdogParamKappa * _xdogParamSigma,
+    _xdogParamTau, _xdogParamSmoothingSigma);
+
+  const auto xdog = thresholdingXDoG(dogResponse, _xdogParamEps, _xdogParamPhi);
+
+  return overlay(
+    xdog, convertColor(quantizeColors(oabf, _phi_q, _nbins),
+                       ColorConverter<double>::Conversion::CIELab_2_rgb));
 }
 
 Mat3d FlowBasedDoG::filterBilateralOrientationAligned(
@@ -108,8 +249,8 @@ Mat3d FlowBasedDoG::quantizeColors(const Mat3d& imageInLab, const double phi_q,
   const auto w = imageInLab.cols;
   const auto h = imageInLab.rows;
   Mat3d out(h, w);
-  for (int y = 0; y < h; y++) {
-    for (int x = 0; x < w; x++) {
+  for (auto y = 0; y < h; y++) {
+    for (auto x = 0; x < w; x++) {
       const auto c = imageInLab(y, x);
 
       const auto qn = std::floor(c[0U] * static_cast<double>(nbins) + 0.5) /
@@ -121,6 +262,50 @@ Mat3d FlowBasedDoG::quantizeColors(const Mat3d& imageInLab, const double phi_q,
     }
   }
   return out;
+}
+
+Mat1d FlowBasedDoG::thresholdingXDoG(const Mat1d& response,
+                                     const double xdogParamEps,
+                                     const double xdogParamPhi) {
+  Mat1d out(response.size());
+  for (int32_t i = 0; i < (response.cols * response.rows); i++) {
+    const auto e = response(i);
+    out(i) = (e > xdogParamEps) ? 1.0 : (1.0 + std::tanh(xdogParamPhi * e));
+  }
+
+  return out;
+}
+
+Mat1d FlowBasedDoG::filterFlowBasedDoG(const Mat1d& img, const Mat2d& tfm,
+                                       const double sigma_e,
+                                       const double sigma_r, const double tau,
+                                       const double sigmaSmoothing) {
+  Mat1d out(img.rows, img.cols);
+
+  // compute DoG
+  detail::fdogAlongGradient(img, out, tfm, sigma_e, sigma_r, tau);
+
+  // smooth along etf
+  Mat1d outSmooth(img.rows, img.cols);
+  detail::smoothAlongFlow(out, outSmooth, tfm, sigmaSmoothing);
+
+  return outSmooth;
+}
+
+Mat3d FlowBasedDoG::overlay(const Mat1d& edges, const Mat3d& image) const {
+  const auto w = edges.cols;
+  const auto h = edges.rows;
+
+  Mat3d t0(h, w);
+  for (auto y = 0; y < h; y++) {
+    for (auto x = 0; x < w; x++) {
+      const auto c = image(y, x);
+      double e     = edges(y, x);
+      t0(y, x)     = {e * c[0U], e * c[1U], e * c[2U]};
+    }
+  }
+
+  return t0;
 }
 
 }  // namespace painty
--- modules/image/test/src/flow_based_dog_test.cpp
+++ modules/image/test/src/flow_based_dog_test.cpp
@@ -17,18 +17,8 @@ TEST(FDoGTest, FDoGTestFunc) {
   // load an image as linear rgb
   painty::io::imRead("./data/test_images/field.jpg", image, true);
 
-  const auto Lab = painty::convertColor(
-    image, painty::ColorConverter<double>::Conversion::rgb_2_CIELab);
-
-  const auto etf = painty::ComputeEdgeTangentFlow(
-    painty::tensor::ComputeTensors(Lab, painty::Mat1d(), 0.0, 0.5, 20.0, 30.0));
-
   painty::FlowBasedDoG fdog;
-  const auto fdogRes = fdog.execute(Lab, etf);
+  const auto fdogRes = fdog.execute(image);
 
-  painty::io::imSave(
-    "./data/test_images/field_oabf.jpg",
-    painty::convertColor(
-      fdogRes, painty::ColorConverter<double>::Conversion::CIELab_2_rgb),
-    true);
+  painty::io::imSave("./data/test_images/field_oabf.jpg", fdogRes, true);
 }
