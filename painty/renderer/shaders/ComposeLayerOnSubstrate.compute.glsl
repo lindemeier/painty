7d7ef8260ee947e830a2183709cfdb27610edd37
tsl <thomas.lindemeier@mojin-robotics.de>
2020-10-12 21:15:32 +0200

    compose shader

--- painty/renderer/shaders/ComposeLayerOnSubstrate.compute.glsl
+++ painty/renderer/shaders/ComposeLayerOnSubstrate.compute.glsl
@@ -11,6 +11,21 @@ layout(binding = 3, r32f) uniform highp readonly image2D tex_V;
 
 uniform ivec2 offset;
 
+highp float coth(in highp float x) {
+  if (x > 20.0) {
+    return 1.0;
+  }
+  if (abs(x) > 0.0) {
+    return cosh(x) / sinh(x);
+  } else {
+    return 1.0 / 0.0;
+  }
+}
+
+highp vec3 coth(in highp vec3 v) {
+  return vec3(coth(v.x), coth(v.y), coth(v.z));
+}
+
 void main() {
   ivec2 gSize = imageSize(tex_R0);
   if (gl_GlobalInvocationID.x < 0 || gl_GlobalInvocationID.y < 0 ||
@@ -21,7 +36,29 @@ void main() {
 
   ivec2 texPos = ivec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
 
-  highp vec4 r0 = imageLoad(tex_R0, texPos);
+  highp vec3 r0 = imageLoad(tex_R0, texPos).rgb;
+  highp vec3 K  = imageLoad(tex_K, texPos).rgb;
+  highp vec3 S  = imageLoad(tex_S, texPos).rgb;
+  highp float d = imageLoad(tex_V, texPos).r;
 
-  imageStore(tex_R0, texPos, vec4(1.0, 1.0, 1.0, 1.0));
+  const float Eps = 10e-6;
+  if ((abs(d) < Eps) || (abs(S.r) < Eps) || (abs(S.g) < Eps) ||
+      (abs(S.b) < Eps)) {
+    return;
+  }
+  highp vec3 K_S = K / S;
+  highp vec3 a   = vec3(1.0, 1.0, 1.0) + K_S;
+
+  highp vec3 asq = a * a;
+
+  highp vec3 b = sqrt(max(asq - vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 0.0)));
+
+  highp vec3 bSh = b * S * d;
+
+  highp vec3 bcothbSh = coth(bSh);
+
+  highp vec3 R =
+    (vec3(1.0, 1.0, 1.0) - r0 * (a - bcothbSh)) / (a - r0 + bcothbSh);
+
+  imageStore(tex_R0, texPos, vec4(R, 1.0));
 }
